c3a8bc1789ea66391b1b1a7fd1c1d13d96c59279
Shimmer93 <zpengac@connect.ust.hk>
2022-11-01 23:04:08 +0800

    use webpack to integrate flask with npm

--- src/main.js
+++ src/main.js
@@ -12,11 +12,6 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-const geometry2 = new THREE.SphereGeometry( 15, 32, 16 );
-const material2 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const sphere = new THREE.Mesh( geometry2, material2 );
-scene.add( sphere );
-
 camera.position.z = 5;
 
 function animate() {
